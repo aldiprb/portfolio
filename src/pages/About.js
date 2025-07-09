2eb5f51fcda04a37a9637675f0ae066e854c3aa6
Aldi <aldipurba141@gmail.com>
2025-07-09 23:29:29 +0700

    Deskripsi perubahan

--- src/pages/About.js
+++ src/pages/About.js
@@ -81,11 +81,6 @@ const About = () => {
           </div>
         </div>
         
-        <div className="decorative-shapes">
-          <div className="shape shape-1"></div>
-          <div className="shape shape-2"></div>
-          <div className="shape shape-3"></div>
-        </div>
       </div>
     </section>
   );
