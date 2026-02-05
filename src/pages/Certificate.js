8995f881d265d28648d7ac0b0b73a3eca763aeea
Aldi <aldipurba141@gmail.com>
2026-02-05 23:22:06 +0700

    update website layout and content

--- src/pages/Certificate.js
+++ src/pages/Certificate.js
@@ -4,6 +4,8 @@ import './Certificate.css';
 import webbasicImage from './images/thumbnails/webbasic.jpg';
 import cloudImage from './images/thumbnails/cloud.jpg';
 import cyberxploreImage from './images/thumbnails/cyberxplore.jpg';
+import itSupportImage from './images/thumbnails/bapenda.png';
+import studentAssociationImage from './images/thumbnails/hmps.png';
 
 const Certificate = () => {
   const certificates = [
@@ -30,6 +32,22 @@ const Certificate = () => {
       description: "the 'CyberXplore Security 2025' webinar likely covered foundational topics in cybersecurity, including the importance of digital security, common cyber threats such as malware and phishing, and ethical practices in the digital world. The event may have also introduced basic tools and techniques used in cybersecurity, such as penetration testing, network analysis, and secure software development practices. Aimed at students and beginners, the webinar was designed to raise awareness and provide practical insights into the latest trends and challenges in the field of cybersecurity.",
       thumbnail: cyberxploreImage, // Ganti dengan path gambar Anda
       pdfLink: "https://drive.google.com/file/d/1PZH7ZWR5xvZpXkN_yg-cUUZ3S77ul6YF/view?usp=sharing" // Ganti dengan link Google Drive Anda
+    },
+    {
+      title: "Software Engineering Student Association Certification",
+      issuer: "Medan State Polytechnic",
+      date: "2025",
+      description: "This certificate is awarded to an individual who has actively served as a member of the Publication and Documentation Division of the Software Engineering Technology Student Association at Politeknik Negeri Medan. The certificate recognizes participation and contributions in managing publications, creating visual and written content, documenting organizational activities, and supporting information dissemination through digital and media platforms. It also reflects the holder’s experience in teamwork, creativity, communication, and organizational responsibility during their period of service.",
+      thumbnail: studentAssociationImage, // Ganti dengan path gambar Anda
+      pdfLink: "https://drive.google.com/drive/u/2/folders/1vjPFwlkuSSQf_Bruu2rsGDvVd7EDOLtN" // Ganti dengan link Google Drive Anda
+    },
+    {
+      title: "IT Support Internship Certification",
+      issuer: "North Sumatra Provincial Government Revenue Agency",
+      date: "2026",
+      description: "An Internship IT Support Certificate is an official document that confirms an individual has successfully completed an internship program as an IT Support. This certificate demonstrates that the intern has gained practical experience in providing technical assistance, maintaining computer systems, troubleshooting hardware and software issues, and supporting users in daily IT operations. Through this internship, the individual develops fundamental skills in information technology, including system maintenance, network support, problem-solving, and professional communication in a workplace environment. The certificate serves as proof of hands-on experience and shows readiness to work in entry-level IT roles or continue professional development in the IT field.",
+      thumbnail: itSupportImage, // Ganti dengan path gambar Anda
+      pdfLink: "https://drive.google.com/drive/u/2/folders/1vjPFwlkuSSQf_Bruu2rsGDvVd7EDOLtN" // Ganti dengan link Google Drive Anda
     }
   ];
 
